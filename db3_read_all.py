9e94075c818cbc322556d63c3936ec46699390ed
Chris Patton <cd.patton@gmail.com>
2024-09-14 10:48:02 -0700

    Add script to read all data from the database

---
+++ db3_read_all.py
@@ -0,0 +1,18 @@
+import asyncio
+
+from db0_setup_database import connect_to_db
+
+
+async def read_all_data():
+    conn = await connect_to_db()
+    rows = await conn.fetch("SELECT * FROM data_table;")
+    for row in rows:
+        print(
+            f"ID={row['id']}, Time={row['time']}, "
+            f"Channel 1={row['channel_1']}, Channel 2={row['channel_2']}, Channel 3={row['channel_3']}"
+        )
+    await conn.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(read_all_data())
